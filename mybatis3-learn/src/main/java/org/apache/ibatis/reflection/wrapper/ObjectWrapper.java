5707780a5d261b6a042831da99b337a35a1fd5fe
mrjimmylin <mrjimmylin@163.com>
2019-09-12 13:42:14 +0800

    update

--- mybatis3-learn/src/main/java/org/apache/ibatis/reflection/wrapper/ObjectWrapper.java
+++ mybatis3-learn/src/main/java/org/apache/ibatis/reflection/wrapper/ObjectWrapper.java
@@ -4,10 +4,11 @@ import org.apache.ibatis.reflection.MetaClass;
 import org.apache.ibatis.reflection.property.PropertyTokenizer;
 
 /**
- * @description 对象包装器接口：基于 {@link org.apache.ibatis.reflection.MetaClass} 工具类，定义对指定对象的操作
  * @author mrjimmylin
+ * @description 对象包装器接口：基于 {@link org.apache.ibatis.reflection.MetaClass} 工具类，定义对指定对象的操作
  * @date 2019/9/9 16:55
  */
+
 public interface ObjectWrapper {
 
     // 获得值 —— propertyTokenizer 相当于键
@@ -17,20 +18,7 @@ public interface ObjectWrapper {
     void set(PropertyTokenizer propertyTokenizer, Object value);
 
     /**
-     * {@link MetaClass#findProperty(String,boolean)}
+     * {@link MetaClass#findProperty(String, boolean)}
      */
     String findProperty(String name, boolean useCamelCaseMapping);
-
-    /**
-     * {@link MetaClass#getGetterNames()}
-     * @return 所有的 getter 方法所对应字段的名称
-     */
-    String[] getGetterNames();
-
-    /**
-     * {@link MetaClass#getSetterNames()}
-     * @return 所有的 setter 方法所对应的字段的名称
-     */
-    String[] getSetterNames();
-
 }
