ab551d9e8346d89fa919c0eb23a625dd3446c44f
mrjimmylin <mrjimmylin@163.com>
2019-10-08 14:38:22 +0800

    update

--- mybatis/src/test/java/org/apache/ibatis/cache/CacheKeyTest.java
+++ mybatis/src/test/java/org/apache/ibatis/cache/CacheKeyTest.java
@@ -1,39 +1,36 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.cache;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.NotSerializableException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class CacheKeyTest {
 
   @Test
   void shouldTestCacheKeysEqual() {
     Date date = new Date();
-    CacheKey key1 = new CacheKey(new Object[] { 1, "hello", null, new Date(date.getTime()) });
-    CacheKey key2 = new CacheKey(new Object[] { 1, "hello", null, new Date(date.getTime()) });
+    CacheKey key1 = new CacheKey(new Object[]{1, "hello", null, new Date(date.getTime())});
+    CacheKey key2 = new CacheKey(new Object[]{1, "hello", null, new Date(date.getTime())});
     assertEquals(key1, key2);
     assertEquals(key2, key1);
     assertEquals(key1.hashCode(), key2.hashCode());
@@ -42,9 +39,9 @@ class CacheKeyTest {
 
   @Test
   void shouldTestCacheKeysNotEqualDueToDateDifference() throws Exception {
-    CacheKey key1 = new CacheKey(new Object[] { 1, "hello", null, new Date() });
+    CacheKey key1 = new CacheKey(new Object[]{1, "hello", null, new Date()});
     Thread.sleep(1000);
-    CacheKey key2 = new CacheKey(new Object[] { 1, "hello", null, new Date() });
+    CacheKey key2 = new CacheKey(new Object[]{1, "hello", null, new Date()});
     assertNotEquals(key1, key2);
     assertNotEquals(key2, key1);
     assertNotEquals(key1.hashCode(), key2.hashCode());
@@ -53,9 +50,9 @@ class CacheKeyTest {
 
   @Test
   void shouldTestCacheKeysNotEqualDueToOrder() throws Exception {
-    CacheKey key1 = new CacheKey(new Object[] { 1, "hello", null });
+    CacheKey key1 = new CacheKey(new Object[]{1, "hello", null});
     Thread.sleep(1000);
-    CacheKey key2 = new CacheKey(new Object[] { 1, null, "hello" });
+    CacheKey key2 = new CacheKey(new Object[]{1, null, "hello"});
     assertNotEquals(key1, key2);
     assertNotEquals(key2, key1);
     assertNotEquals(key1.hashCode(), key2.hashCode());
@@ -80,10 +77,10 @@ class CacheKeyTest {
 
   @Test
   void shouldTestCacheKeysWithBinaryArrays() {
-    byte[] array1 = new byte[] { 1 };
-    byte[] array2 = new byte[] { 1 };
-    CacheKey key1 = new CacheKey(new Object[] { array1 });
-    CacheKey key2 = new CacheKey(new Object[] { array2 });
+    byte[] array1 = new byte[]{1};
+    byte[] array2 = new byte[]{1};
+    CacheKey key1 = new CacheKey(new Object[]{array1});
+    CacheKey key2 = new CacheKey(new Object[]{array2});
     assertEquals(key1, key2);
   }
 
@@ -104,11 +101,11 @@ class CacheKeyTest {
   }
 
   private static <T> T serialize(T object) throws Exception {
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      new ObjectOutputStream(baos).writeObject(object);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    new ObjectOutputStream(baos).writeObject(object);
 
-      ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-      return (T) new ObjectInputStream(bais).readObject();
+    ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+    return (T) new ObjectInputStream(bais).readObject();
   }
 
 }
